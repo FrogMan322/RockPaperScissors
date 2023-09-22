3ea502a5ce3c6447a22eeabd0630a887869a50c2
FrogMan322 <gojkov.nemanja69@gmail.com>
2023-09-22 10:35:16 +0200

    Gameplay update patch 2.0 XD

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const computerChouse = ["rock", "paper", "scissors"];
 const winnerPlayer = document.querySelector(".winner__player");
 const winnerComputer = document.querySelector(".winner__computer");
+const modalOverlay = document.querySelector(".overlay");
+const resetBtn = document.querySelector(".resetBtn");
+const scoreBoardPc = document.getElementById("pc__score");
+const scoreBoardPlayer = document.getElementById("player__score");
+
 const btns = Array.from(document.querySelectorAll(".choise__container"));
 const pcRandom = () => {
   const random = Math.floor(Math.random() * computerChouse.length);
@@ -10,8 +15,6 @@ let playerScore = 0;
 let computerScore = 0;
 btns.forEach((e) => {
   e.addEventListener("click", (event) => {
-    const scoreBoardPc = document.getElementById("pc__score");
-    const scoreBoardPlayer = document.getElementById("player__score");
     const playerChose = event.target.dataset.choise;
     const computerChose = pcRandom();
 
@@ -36,9 +39,23 @@ btns.forEach((e) => {
     scoreBoardPc.textContent = `PC Score:${computerScore}`;
     scoreBoardPlayer.textContent = `Player Score:${playerScore}`;
     if (playerScore === 10) {
+      modalOverlay.classList.add("active");
       winnerPlayer.classList.add("active");
     } else if (computerScore === 10) {
+      modalOverlay.classList.add("active");
       winnerComputer.classList.add("active");
     }
   });
 });
+
+function playAgain() {
+  modalOverlay.classList.remove("active");
+  winnerPlayer.classList.remove("active");
+  winnerComputer.classList.remove("active");
+  scoreBoardPc.textContent = `PC Score:0`;
+  scoreBoardPlayer.textContent = `Player Score:0`;
+  playerScore = 0;
+  computerScore = 0;
+}
+
+resetBtn.addEventListener("click", playAgain);
